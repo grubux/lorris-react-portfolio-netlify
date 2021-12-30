ac9c573694361faf1448b8d60ab0b2d59c62471b
Lorris Lehuede <lehuedelorris@gmail.com>
2021-12-30 08:35:58 +0100

    add basename to router for deployment

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Main from "./containers/Main";
 import Company1 from "./containers/Company1";
 const App = () => {
   return (
-    <Router>
+    <Router basename="/lorris-react-portfolio">
       <Routes>
         <Route exact path="/" element={<Main />} />
         <Route path="/company1" element={<Company1 />} />
