67f2b9788cf951c8cdb8353366bf4c1968857159
Lorris Lehuede <lehuedelorris@gmail.com>
2022-01-04 08:30:04 +0100

    Updates

---
+++ precache-manifest.75ee4249be041bf28a7c60169d6b0704.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a6dbcb10f41de8e9a331c82228220280",
+    "url": "/lorris-react-portfolio/index.html"
+  },
+  {
+    "revision": "3b4941bb771109309e7e",
+    "url": "/lorris-react-portfolio/static/css/2.9c389199.chunk.css"
+  },
+  {
+    "revision": "6f95876add900c359419",
+    "url": "/lorris-react-portfolio/static/css/main.4588be86.chunk.css"
+  },
+  {
+    "revision": "3b4941bb771109309e7e",
+    "url": "/lorris-react-portfolio/static/js/2.4e66804b.chunk.js"
+  },
+  {
+    "revision": "f38308786670e030dbb024eae0a4f8b8",
+    "url": "/lorris-react-portfolio/static/js/2.4e66804b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6f95876add900c359419",
+    "url": "/lorris-react-portfolio/static/js/main.faba1644.chunk.js"
+  },
+  {
+    "revision": "f5470a5bb6dddf7bbf8f",
+    "url": "/lorris-react-portfolio/static/js/runtime-main.84963664.js"
+  },
+  {
+    "revision": "63270399fe72c175ba89c90ec903dc4b",
+    "url": "/lorris-react-portfolio/static/media/mainLogo.63270399.png"
+  }
+]);
