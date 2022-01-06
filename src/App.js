b67162a5d560853c5d141c78faed00acf6f1291d
Lorris Lehuede <lehuedelorris@gmail.com>
2022-01-06 17:44:48 +0100

    add UmanovaSA page

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Main from "./containers/Main";
 import Archlet from "./containers/Archlet";
+import UmanovaSA from "./containers/UmanovaSA";
 const App = () => {
   return (
     <Router>
       <Routes>
+        <Route exact path="/" element={<Main />} />
         <Route path="/archlet" element={<Archlet />} />
-        <Route path="/" element={<Main />} />
+        <Route path="/umanovasa" element={<UmanovaSA />} />
       </Routes>
     </Router>
   );
